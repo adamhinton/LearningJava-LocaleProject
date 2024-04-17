c76e0863ab522329caea1779ad5dee4bdef16040
adamd <adam.d.hinton@gmail.com>
2024-04-17 10:53:49 -0500

    setting default Locale

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -5,7 +5,10 @@ import java.util.Locale;
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+
         System.out.println("Default Locale = "+Locale.getDefault());
+
 
 
 
